859eea3fdd41918218dd12b93577123cbf33a243
Pujun Bhatnagar <pbhatnagar3@gatech.edu>
2014-08-11 17:33:49 -0700

    added cryptographic powgit add .

---
+++ src/Pow.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+/**
+ * Testing one of the methods that is used in Cryptography for finding the very large powers.
+ * @author pbhatnagar
+ * If you have any questions or comments, please feel free to contact
+ * me at pbhatnagar3@gatech.edu
+ *
+ * MAY THE FORCE OF COMPILER BE WITH YOU :D
+ */
+public class Pow {
+	
+	public static int pow(int base, int exp){
+		int result = 1;
+		while(exp > 0){
+			if((exp & 1) == 1)
+				result *= base;
+			exp >>= 1;
+			base *= base;
+		}
+		return result;
+	}
+	
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter the value of the base");
+		int base = scan.nextInt();
+		System.out.println("enter the value of the exponent");
+		int exponent = scan.nextInt();
+		System.out.println("here is the answer to the computation, my lord, " + pow(base, exponent));
+	}
+
+}
